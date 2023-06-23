d1a05b57876614372caa25e0ddaf9850ff51538f
jdelarubia <jdelarubia@gmail.com>
2023-06-23 22:52:52 +0200

    some tests

---
+++ tests/test.py
@@ -0,0 +1,30 @@
+"""test_generate.py
+"""
+
+import unittest
+import code.app as app
+from code.matrix import Matrix
+
+
+class TestGeneration(unittest.TestCase):
+    def test_type(self):
+        m = Matrix(0)
+        expected = str(m)
+        current = type(m)
+        self.assertEqual(current, expected, "")
+
+    def test_single_dimension(self):
+        m = Matrix(1)
+        expected = 1
+        current = len(m)
+        self.assertEqual(current, expected, f"length should be {expected}")
+
+    def test_two_dimensions(self):
+        m = Matrix(2)
+        expected = 4
+        current = len(m)
+        self.assertEqual(current, expected, f"length should be {expected}")
+
+
+if __name__ == "__main__":
+    unittest.main()
