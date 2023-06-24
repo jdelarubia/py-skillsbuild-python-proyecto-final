64cc4370f7697a868b88ae4a9c6a756863ced67c
jdelarubia <jdelarubia@gmail.com>
2023-06-24 11:50:39 +0200

    refactor sample code

--- code/app.py
+++ code/app.py
@@ -1,8 +1,25 @@
 """app.py
+Sample code to test our Matrix general functionality.
 """
+
 from matrix import Matrix
 
 if __name__ == "__main__":
     m = Matrix(3)
-    print(type(m))
-    print(m.matrix)
+
+    print("representación de la matriz")
+    print(repr(m))
+
+    row = 2
+    print(f"suma de la fila {row}:", m.sum_row(row))
+
+    col = 3
+    print(f"suma de la columna {col}:", m.sum_col(col))
+
+    print()
+    print(f"sumas de todas las filas: {m._sum_rows}")
+    print(f"sumas de todas las columnas: {m._sum_columns}")
+
+    print()
+    print(f"suma de todas las filas: {m.sum_rows}")
+    print(f"suma de todas las columnas: {m.sum_columns}")
