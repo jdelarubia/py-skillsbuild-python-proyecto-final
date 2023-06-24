0125b490832ac7a5f5a7f4c8fec6a4d7dbbe2ada
jdelarubia <jdelarubia@gmail.com>
2023-06-24 11:46:59 +0200

    store the sum of the columns

--- code/matrix.py
+++ code/matrix.py
@@ -9,10 +9,15 @@ class Matrix:
         self._dim = dim  # matrix dimension
         self._matrix = [[randint(0, 9) for i in range(dim)] for j in range(dim)]
         self._sum_rows = [self.sum_row(row + 1) for row in range(dim)]
+        self._sum_columns = [self.sum_col(col + 1) for col in range(dim)]
+
     @property
     def sum_rows(self) -> int:
         return sum(self._sum_rows)
 
+    @property
+    def sum_columns(self) -> int:
+        return sum(self._sum_columns)
 
     @property
     def matrix(self) -> list:
