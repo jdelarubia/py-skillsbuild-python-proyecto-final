5cd612cfa2726461cbb73b6e89e25f15d5f6ddf0
jdelarubia <jdelarubia@gmail.com>
2023-06-26 18:05:45 +0200

    refactor rename test methods

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -11,20 +11,14 @@ class TestMatrixFuncionality(unittest.TestCase):
         self.test_matrix = Matrix(2)
         self.test_matrix.matrix = [[1, 2], [3, 4]]
 
-    def test_type(self):
-        """m is instance of Matrix"""
-        m = Matrix(0)
-        expected = Matrix
-        self.assertIsInstance(m, expected, f"type should be {expected}")
-
-    def test_empty_matrix(self):
+    def test_empty_matrix_should_return_length_zero(self):
         """len is 0 for empty matrix"""
         m = Matrix(0)
         expected = 0
         current = len(m)
         self.assertEqual(current, expected, f"length should be {expected}")
 
-    def test_matrix_dimension(self):
+    def test_multiple_length_of_matrices(self):
         """len is n*n for n-dimension matrix"""
         for dim in range(1, 7):
             m = Matrix(dim)
@@ -32,19 +26,19 @@ class TestMatrixFuncionality(unittest.TestCase):
             current = len(m)
             self.assertEqual(current, expected, f"length should be {expected}")
 
-    def test_sum_all_rows(self):
+    def test_sum_of_all_rows_property(self):
         """test sum_all_rows"""
         expected = 10
         current = self.test_matrix.sum_all_rows
         self.assertEqual(current, expected, f"sum should be {expected}")
 
-    def test_sum_all_columns(self):
+    def test_sum_of_all_columns_property(self):
         """test sum_all_columns"""
         expected = 10
         current = self.test_matrix.sum_all_columns
         self.assertEqual(current, expected, f"sum should be {expected}")
 
-    def test_sum_rows(self):
+    def test_sum_rows_property(self):
         """test sum_rows"""
         expected = 3
         current = self.test_matrix.sum_row(1)
@@ -53,7 +47,7 @@ class TestMatrixFuncionality(unittest.TestCase):
         current = self.test_matrix.sum_row(2)
         self.assertEqual(current, expected, f"sum should be {expected}")
 
-    def test_sum_columns(self):
+    def test_sum_columns_property(self):
         """test sum_columns"""
         expected = 4
         current = self.test_matrix.sum_col(1)
